8164538d2eeeb125237440aac44003c15b96a019
shery1999 <109337372+shery1999@users.noreply.github.com>
2022-11-22 16:33:21 +0500

    [sensitivity updated]

---
+++ app/Models/Processing.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Processing extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'unprocessed_grading_id',
+        'first_storage_id',
+        'user_id',
+        'store_id',
+        'description',
+        'workshop_id',
+        'status',
+
+    ];
+
+    public function workshop()
+    {
+        return $this->hasOne(Workshop::class, 'id', 'workshop_id');
+    }
+} 
