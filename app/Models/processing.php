85ae7363d52210aa538166973aa2b4ab78354558
shery1999 <109337372+shery1999@users.noreply.github.com>
2022-11-22 13:08:14 +0500

    [sensitivity updated]

--- app/Models/processing.php
+++ app/Models/processing.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class processing extends Model
+class Processing extends Model
 {
     use HasFactory;
     protected $fillable = [
